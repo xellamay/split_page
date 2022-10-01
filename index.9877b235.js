a8501d3da9d38dc0de546e9c8b1f8d26b20ec445
Alena <xellatroy@gmail.com>
2022-10-01 16:02:22 +0300

    Updates

---
+++ index.9877b235.js
@@ -0,0 +1,2 @@
+const sections=document.querySelectorAll(".split-viewer__section");function clearClass(){sections.forEach((e=>{e.classList.remove("split-viewer__section_active")}))}sections.forEach((e=>{e.addEventListener("mouseenter",(function(){clearClass(),e.classList.add("split-viewer__section_active")}))}));
+//# sourceMappingURL=index.9877b235.js.map
