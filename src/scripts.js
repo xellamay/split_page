f317a0c09d36fff68e05cceb036f9911a7daaee3
Alena <xellatroy@gmail.com>
2022-10-01 21:13:00 +0300

    Рефакторинг JS

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,21 @@
-const sections = document.querySelectorAll(".split-viewer__section");
+document.addEventListener("DOMContentLoaded", init);
 
-function clearClass() {
+function init() {
+  const root = document.querySelector(".split-viewer");
+  const sections = document.querySelectorAll(".split-viewer__section");
+
+  const clearClass = () => {
     sections.forEach((section) => {
-        section.classList.remove("split-viewer__section_active")
+        section.classList.remove("split-viewer__section_active");
     })
 }
 
-sections.forEach((section) =>{
-    section.addEventListener("mouseenter", function() {
-        clearClass();
-        section.classList.add("split-viewer__section_active")
-    })
-})
+  sections.forEach((section) => {
+      section.addEventListener("mouseenter", () => {
+          clearClass();
+          section.classList.add("split-viewer__section_active");
+      })
+  })
 
-
+  root.addEventListener("mouseleave", clearClass);
+}
